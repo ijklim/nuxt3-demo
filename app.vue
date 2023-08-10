1862cc1b7480c77353adf90ba7a10c7d819eafdc
ivan-aim <77643196+ivan-aim@users.noreply.github.com>
2023-08-10 14:37:35 -0400

    NuxtLink and route validation

--- app.vue
+++ app.vue
@@ -1,14 +1,31 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <div class="content">
+      <NuxtPage />
+    </div>
 
-    <a href="/">Home</a>
-    &nbsp;
-    &nbsp;
-    &nbsp;
-    <a href="/about">About Us</a>
+    <div class="links">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+      <NuxtLink to="/products/5">Product: 5</NuxtLink>
+      <NuxtLink to="/products/118">Product: 118</NuxtLink>
+      <NuxtLink to="/products/error">Product: error</NuxtLink>
+    </div>
   </NuxtLayout>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+  .content {
+    min-height: 85vh;
+    padding: 10px;
+  }
+
+  .links {
+    text-align: center;
+    width: 100%;
+
+    a {
+      margin: 0px 10px;
+    }
+  }
 </style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,13 @@
+<script setup>
+  const route = useRoute();
+
+  definePageMeta({
+    validate: async (route) => {
+      return /^\d+$/.test(route.params.id);
+    },
+  });
+</script>
+
+<template>
+  <h1>Product Page, id: {{ route.params.id }}</h1>
+</template>
